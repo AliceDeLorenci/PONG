a318591c9ac371baadddfc9882e9585543534f47
Guerreiro51 <alessandrog54@usp.br>
2021-11-06 21:18:24 -0300

    Fixed WSL

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,7 @@
 #include "Pong.h"
 #include "spdlog/spdlog.h"
 
-int main(int argc, char* argv[]) {
-    
-    bool vsync = false;
-    bool cohesion = false;
-
-    struct utsname buf;
-    memset(&buf, 0, sizeof buf);
-
-    int ret = uname(&buf);
-
-    if ((ret == 0) && (strstr(buf.release, "WSL"))) {
-        vsync = true;
-        cohesion = true;
-    }
-
-#if SERVER
+static void init(std::unique_ptr<Pong::Pong>& pong) {
 #ifndef _DEBUG
     spdlog::set_level(spdlog::level::info);
     spdlog::set_pattern("[%H:%M:%S] [%^%l%$] %v");
@@ -27,9 +12,18 @@ int main(int argc, char* argv[]) {
     spdlog::set_level(spdlog::level::trace);
     spdlog::set_pattern("[%H:%M:%S] [%^%l%$] [thread %t] %v");
 #endif
+    struct utsname sys;
+    memset(&sys, 0, sizeof sys);
+    int ret = uname(&sys);
+    bool vsync = ((ret == 0) && (strstr(sys.release, "WSL"))); // fix for WSL on windows 10
 
-    spdlog::info("Initializing Server...");
+    if (pong->Construct(640, 360, 2, 2, false, vsync, false)) {  // Tela de tamanho 640x320 com 'pixels' formado por 2x2 pixels
+        pong->Start();
+    }
+}
 
+int main(int argc, char* argv[]) {
+#if SERVER
     std::unique_ptr<Pong::Pong> pong;
     if (argc == 3) {
         pong = std::make_unique<Pong::Pong>("127.0.0.1", argv[1], argv[2]);
@@ -39,25 +33,11 @@ int main(int argc, char* argv[]) {
         spdlog::error("Usage: ./Server <UDP port> <TCP port>");
         return 1;
     }
-
-    if (pong->Construct(640, 360, 2, 2, false, false, false)) {  // Tela de tamanho 640x320 com 'pixels' formado por 2x2 pixels
-        pong->Start();
-    }
-
+    spdlog::info("Initializing Server...");
+    init(pong);
     spdlog::info("Server finished shutting down. See you next time!");
     return 0;
-
 #elif CLIENT
-#ifndef _DEBUG
-    spdlog::set_level(spdlog::level::info);
-    spdlog::set_pattern("[%H:%M:%S] [%^%l%$] %v");
-#else
-    spdlog::set_level(spdlog::level::trace);
-    spdlog::set_pattern("[%H:%M:%S] [%^%l%$] [thread %t] %v");
-#endif
-
-    spdlog::info("Initializing Client...");
-
     std::unique_ptr<Pong::Pong> pong;
     if (argc == 4) {
         pong = std::make_unique<Pong::Pong>(argv[1], argv[2], argv[3]);
@@ -67,29 +47,15 @@ int main(int argc, char* argv[]) {
         spdlog::error("Usage: ./Client <ip_address> <UDP port> <TCP port>");
         return 1;
     }
-
-    if (pong->Construct(640, 360, 2, 2, false, false, false)) {  // Tela de tamanho 640x360 com 'pixels' formado por 2x2 pixels
-        pong->Start();
-    }
-
+    spdlog::info("Initializing Client...");
+    init(pong);
     spdlog::info("Client finished shutting down. See you next time!");
     return 0;
 
 #elif OFFLINE
-#ifndef _DEBUG
-    spdlog::set_level(spdlog::level::info);
-    spdlog::set_pattern("[%H:%M:%S] [%^%l%$] %v");
-#else
-    spdlog::set_level(spdlog::level::trace);
-    spdlog::set_pattern("[%H:%M:%S] [%^%l%$] [thread %t] %v");
-#endif
-
-    spdlog::info("Initializing Offline Game...");
-
     std::unique_ptr<Pong::Pong> pong = std::make_unique<Pong::Pong>();
-    if (pong->Construct(640, 360, 2, 2, false, false, false))  // Tela de tamanho 640x360 com 'pixels' formado por 2x2 pixels
-        pong->Start();
-
+    spdlog::info("Initializing Offline Game...");
+    init(pong);
     spdlog::info("Game finished shutting down. See you next time!");
     return 0;
 
